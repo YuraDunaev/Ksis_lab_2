c2d2b46a654b582b29b9a3b7ba9b973bebc42998
YuraDunaev <101969674+YuraDunaev@users.noreply.github.com>
2022-03-29 13:26:19 +0300

    Add files via upload

---
+++ Ksis_lab_2 - Copy/Ksis_lab_2/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Ksis_lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(126, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1396, 53);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "google.com";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Calibri", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(126, 184);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1396, 608);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(1528, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 71);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Trace";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Calibri", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(1528, 156);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(270, 71);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(2142, 1173);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Ksis_lab_2 - Copy/Ksis_lab_2/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Ksis_lab_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Tracert(string remoteHost)
+        {
+            if (string.IsNullOrEmpty(remoteHost))
+            {
+                throw new ArgumentException($"'{nameof(remoteHost)}' cannot be null or empty.", nameof(remoteHost));
+            }
+
+            byte[] data = new byte[1024];
+            int recv = 0;
+            Socket host = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Icmp);
+            IPAddress addres;
+            try
+            {
+                var @var = Dns.GetHostEntry(remoteHost);
+                addres = Dns.GetHostEntry(remoteHost).AddressList.First(x => x.AddressFamily == AddressFamily.InterNetwork);
+            }catch (Exception ex)
+            {
+                richTextBox1.Text += "Error: Unknown host name\n";
+                throw new Exception("Unknown host name");
+            }
+            IPEndPoint iep = new IPEndPoint(addres, 0);
+            EndPoint ep = iep;
+
+            ICMP packet = new ICMP(0x08, 0x00);
+     
+            host.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReceiveTimeout, 3000);
+
+            int badcount = 0;
+
+            for (int i = 1; i < 256; i++)
+            {
+                host.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.IpTimeToLive, i);
+
+                DateTime timestart = DateTime.Now;
+                host.SendTo(packet.GetBytes(), packet.PacketSize, SocketFlags.None, iep);
+                try
+                {
+                    data = new byte[1024];
+                    recv = host.ReceiveFrom(data, ref ep);
+                    TimeSpan timestop = DateTime.Now - timestart;
+
+                    ICMP response = new ICMP(data, recv);
+
+                    if (response.Type == 11)
+                    {
+                        richTextBox1.Text += i + ":\t" + ep.ToString() + "\t" + timestop.Milliseconds.ToString() + "мс\n";
+                    }
+
+                    if (response.Type == 0)
+                    {
+                        richTextBox1.Text += "[" + ep.ToString() + "]" + " достигнут за " + i + " прыжков, " + (timestop.Milliseconds.ToString()) + "мс\n";
+                        break;
+                    }
+
+                    badcount = 0;
+                }
+                catch (SocketException exeption)
+                {
+                    richTextBox1.Text += i + ": нет ответа от " + ep + " (" + iep + ") - ttl:" + Convert.ToString(host.Ttl) + "\n";
+                    badcount++;
+
+                    if (badcount == 3)
+                    {
+                        richTextBox1.Text += "Не удалось установить соединение\n";
+                        break;
+                    }
+                }
+            }
+            host.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Tracert(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+    }
+
+    class ICMP
+    {
+        private const int IcmpHeaderSize = 8;
+        private const int IpHeaderSize = 20;
+        private const int MaxIpHeaderSize = 24;
+
+        public byte Type;
+        public byte Code;
+        public ushort Checksum;
+        public int Size;
+        private int MessageSize;
+        public int PacketSize;
+        public byte[] Message = new byte[1024];
+        public byte[] Data;
+
+        public ICMP(byte type, byte code)
+        {
+             
+            Type = type;
+            Code = code;
+            Buffer.BlockCopy(BitConverter.GetBytes(1), 0, Message, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(1), 0, Message, 2, 2);
+            Data = Encoding.ASCII.GetBytes("hello");
+            Buffer.BlockCopy(Data, 0, Message, 4, Data.Length); // 4 from upper block copy 
+            Size = Data.Length + IcmpHeaderSize;
+            Checksum = 0;
+            Checksum = GetChecksum();
+            MessageSize = Data.Length;// + "hello".Length;
+            PacketSize = MessageSize + IcmpHeaderSize;//
+        }
+
+        public ICMP(byte[] rawIpPacket, int size)
+        {
+            rawIpPacket = rawIpPacket.Skip(IpHeaderSize).ToArray();
+            Type = rawIpPacket[0];
+            Code = rawIpPacket[1];
+            Data = Encoding.ASCII.GetBytes("test packet");
+            //Checksum = BitConverter.ToUInt16(rawIpPacket, 2);
+            Checksum = GetChecksum();
+            MessageSize = Data.Length;
+            Buffer.BlockCopy(rawIpPacket, MaxIpHeaderSize, Message, 0, MessageSize);
+        }
+
+        public byte[] GetBytes()
+        {
+            MessageSize = Data.Length;// + "test packet".Length;
+            byte[] data = new byte[1024];
+            Buffer.BlockCopy(BitConverter.GetBytes(Type), 0, data, 0, 1);
+            Buffer.BlockCopy(BitConverter.GetBytes(Code), 0, data, 1, 1);
+            Buffer.BlockCopy(BitConverter.GetBytes(Checksum), 0, data, 2, 2);
+            Buffer.BlockCopy(Data, 0, data, 8, MessageSize); //from upper block copy
+            return data;
+        }
+
+        public ushort GetChecksum()
+        {
+            uint chcksm = 0;
+            byte[] data = GetBytes();
+            int packetsize = MessageSize + IcmpHeaderSize;
+            int index = 0;
+
+            while (index < packetsize)
+            {
+                chcksm += Convert.ToUInt32(BitConverter.ToUInt16(data, index));
+                index += 2;
+            }
+            chcksm = (chcksm >> 16) + (chcksm & 0xffff);// 4+4 = 8; 24-8 = 16
+            chcksm += (chcksm >> 16);
+            return (ushort)~chcksm;
+        }
+    }
+}
